7cebf5ac07593cb7887fa94e83f62743dd0ce4d6
Sanjid <dsanjid@gmail.com>
2023-08-23 14:44:54 -0400

    working on followers controller

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -3,6 +3,7 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
+    "Accept": "application/json",
     "X-CSRF-Token": csrfToken,
 
     ...options.headers
